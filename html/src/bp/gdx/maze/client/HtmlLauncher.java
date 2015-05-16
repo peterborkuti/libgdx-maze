87b649906ac05575893c04052cdfd1041ae9558b
Peter Borkuti <peter@debian>
2015-05-16 19:55:28 +0200

    initial project

---
+++ html/src/bp/gdx/maze/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package bp.gdx.maze.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import bp.gdx.maze.MazeGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MazeGame();
+        }
+}
